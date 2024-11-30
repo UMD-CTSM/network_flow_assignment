58c747733588382cbed357098a38300edafa2808
Nathan <nczh@umd.edu>
2024-11-30 15:08:17 -0500

    Added intra-zone freights

--- src/network_analysis/flow_assignment.py
+++ src/network_analysis/flow_assignment.py
@@ -31,29 +31,42 @@ class FlowAssignment:
 
   def faf_flows_to_df(self, faf_flows : pd.DataFrame, SUM_COLUMN) -> pd.DataFrame:
     faf_flows = faf_flows.astype(int)
+    
+    # Flip flows
     larger_first_flows = faf_flows.dms_orig > faf_flows.dms_dest
     faf_flows.loc[larger_first_flows, ['dms_orig', 'dms_dest']] = faf_flows.loc[larger_first_flows, ['dms_dest', 'dms_orig']].values
     faf_flows_by_pair = faf_flows.groupby(['dms_orig', 'dms_dest']).agg({SUM_COLUMN: ['sum']})
     faf_flows_by_pair.columns = faf_flows_by_pair.columns.map('_'.join)
     
-    # Filter out self-flows
-    faf_flows_by_pair = faf_flows_by_pair.reset_index()
-    faf_flows_by_pair = faf_flows_by_pair.loc[faf_flows_by_pair.dms_orig != faf_flows_by_pair.dms_dest].set_index(['dms_orig', 'dms_dest'])
-
     # Filter out flows with no value
     faf_flows_by_pair = faf_flows_by_pair.loc[faf_flows_by_pair[SUM_COLUMN + '_sum'] > 0]
     return faf_flows_by_pair
   
   def merge_segments_flows(self, path_segments : pd.DataFrame, faf_flows_by_pair : pd.DataFrame, SUM_COLUMN) -> pd.DataFrame:
-    path_segments_w_traffic = path_segments.join(faf_flows_by_pair)
-    reversed_segs = path_segments_w_traffic.seg_start > path_segments_w_traffic.seg_end
-    reversed_seg_ends = path_segments_w_traffic.loc[reversed_segs, 'seg_end']
-    reversed_seg_starts = path_segments_w_traffic.loc[reversed_segs, 'seg_start']
+
+    # Flip segments
+    reversed_segs = path_segments.seg_start > path_segments.seg_end
+    path_segments.loc[reversed_segs, ['seg_start', 'seg_end']] = path_segments.loc[reversed_segs, ['seg_end', 'seg_start']]
     
-    path_segments_w_traffic.loc[reversed_segs, 'seg_start'] = reversed_seg_ends
-    path_segments_w_traffic.loc[reversed_segs, 'seg_end'] = reversed_seg_starts
+    path_segments_w_traffic = path_segments.join(faf_flows_by_pair)
+    
     segment_traffic = path_segments_w_traffic.groupby(['seg_start', 'seg_end']).agg({SUM_COLUMN + '_sum':['sum']})
     return segment_traffic
+
+  def add_intra_segment_flows(self, segment_traffic : pd.DataFrame, faf_flows_by_pair : pd.DataFrame, SUM_COLUMN) -> pd.DataFrame:
+    intra_zone_flows = faf_flows_by_pair.loc[faf_flows_by_pair.index.get_level_values('dms_orig') == faf_flows_by_pair.index.get_level_values('dms_dest')]
+    intra_zone_flows = intra_zone_flows.reset_index(1, drop=True)[SUM_COLUMN + '_sum']
+    display(segment_traffic.loc[segment_traffic.isna()])
+    segment_traffic = segment_traffic.reset_index()
+    segment_traffic = segment_traffic.join(intra_zone_flows, on='seg_start').rename(columns={SUM_COLUMN + '_sum': SUM_COLUMN + '_sum_start'})
+    segment_traffic = segment_traffic.join(intra_zone_flows, on='seg_end').rename(columns={SUM_COLUMN + '_sum': SUM_COLUMN + '_sum_end'})
+    segment_traffic = segment_traffic.fillna(0)
+    segment_traffic['new_sum'] = segment_traffic['sum'] + (
+      segment_traffic[SUM_COLUMN + '_sum_start'] + segment_traffic[SUM_COLUMN + '_sum_end']
+    ) / 2A
+    display(segment_traffic.loc[segment_traffic.isna().any(axis=1)])
+    segment_traffic = segment_traffic.set_index(['seg_start', 'seg_end'])
+    return segment_traffic['new_sum']
 
   def apply_flows(self, railnet : nx.Graph, segment_traffic : pd.DataFrame, SUM_COLUMN) -> nx.Graph:
     for (start, end), traffic in segment_traffic.iterrows():
@@ -63,10 +76,12 @@ class FlowAssignment:
   def run(self, input_flows : pd.DataFrame = None, SUM_COLUMN = '', faf_flows_by_pair=None):
     if faf_flows_by_pair is None:
       faf_flows_by_pair = self.faf_flows_to_df(input_flows, SUM_COLUMN)
-    return self.merge_segments_flows(self.path_segments, faf_flows_by_pair, SUM_COLUMN)[SUM_COLUMN + '_sum']['sum']
-
-    railnet = self.apply_flows(railnet, segment_traffic)
-    return railnet
+    segment_flows = self.merge_segments_flows(self.path_segments, faf_flows_by_pair, SUM_COLUMN)[SUM_COLUMN + '_sum']['sum']
+    
+    segment_flows = self.add_intra_segment_flows(segment_flows, faf_flows_by_pair, SUM_COLUMN)
+    
+    return segment_flows
+    # railnet = self.apply_flows(railnet, segment_traffic)
 
 if __name__ == "__main__":
   import argparse
